b2cb25b52d0c1c2644c6fd2c0cc716c78d5b0645
saintmaxi <36748278+saintmaxi@users.noreply.github.com>
2022-07-28 19:29:03 -0500

    update

--- scripts/workshopEthers.js
+++ scripts/workshopEthers.js
@@ -120,7 +120,7 @@ const addListing = async () => {
         }
     }
     catch (error) {
-        if ((error.message).includes("You are not Authorized for this ERC20 Contract!")) {
+        if ((error.message).includes("You are not Authorized")) {
             await displayErrorMessage(`You are not authorized to add listings for $SOULS!`);
         }
         else if ((error.message).includes("User denied transaction signature")) {
@@ -240,7 +240,7 @@ const modifyListing = async () => {
         }
     }
     catch (error) {
-        if ((error.message).includes("You are not Authorized for this ERC20 Contract!")) {
+        if ((error.message).includes("You are not Authorized")) {
             await displayErrorMessage(`You are not authorized to add listings for $SOULS!`);
         }
         else if ((error.message).includes("User denied transaction signature")) {
